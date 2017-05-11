49b7608661e5ef539aa50ff98b49aae9bf1d0f47
cpetot <claude.petot@gmail.com>
2017-05-11 13:44:51 +0200

    Refactoring des services
    - Suppression des interfaces

--- src/main/java/com/cpetot/services/UserService.java
+++ src/main/java/com/cpetot/services/UserService.java
@@ -2,16 +2,68 @@ package com.cpetot.services;
 
 import java.util.List;
 
-import com.cpetot.entities.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.Assert;
+
 import com.cpetot.entities.Movie;
+import com.cpetot.entities.User;
+import com.cpetot.exceptions.UserNameNotFoundException;
+import com.cpetot.exceptions.UserNotAllowedToWatchException;
+import com.cpetot.repository.MovieRepository;
+import com.cpetot.repository.UserRepository;
 
-public interface UserService {
+@Service
+public class UserService {
 
-	List<User> findAll();
+	@Autowired
+	private UserRepository userRepository;
 
-	void addToWatchList(String username, String movieTitle);
+	@Autowired
+	private MovieRepository movieRepository;
 
-	List<Movie> getWatchList(String username);
+	@Autowired
+	private MovieService movieService;
 
-	User getUser(String username);
+	public List<User> findAll() {
+		return userRepository.findAll();
+	}
+
+	@Transactional
+	public void addToWatchList(String username, String movieTitle) {
+		User user = this.getUser(username);
+		Movie movie = movieService.getByTitle(movieTitle);
+
+		if (user.getAge() < movie.getRating().minAge()) {
+			throw new UserNotAllowedToWatchException(user.getAge(), movie.getRating().name());
+		}
+
+		movie.setUser(user);
+		userRepository.save(user);
+	}
+
+	public List<Movie> getWatchList(String username) {
+		User user = this.getUser(username);
+		return user.getWatchList();
+	}
+
+	public User getUser(String username) {
+		Assert.notNull(username, "Username must not be null");
+		User user = this.userRepository.findByUsername(username);
+		if (user == null) {
+			throw new UserNameNotFoundException(username);
+		}
+		return user;
+	}
+
+	private boolean movieInWatchlist(List<Movie> watchList, Movie movie) {
+		for (Movie currentMovie : watchList) {
+			if (currentMovie.getTitle().equalsIgnoreCase(movie.getTitle())) {
+				return true;
+			}
+		}
+		return false;
+	}
+
 }
