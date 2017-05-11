b0ba5400d7f8b23b15215cfb1c8dd2c82fb7b5a9
cpetot <claude.petot@gmail.com>
2017-05-11 13:52:31 +0200

    Ajout de Swagger pour la visualisation des services
    - Ajout de Swagger et Swagger-UI
    - Redirection de / vers la page Swagger

--- src/main/java/com/cpetot/Application.java
+++ src/main/java/com/cpetot/Application.java
@@ -2,8 +2,10 @@ package com.cpetot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(WebConfiguration.class)
 public class Application {
 
 	public static void main(String[] args) {
