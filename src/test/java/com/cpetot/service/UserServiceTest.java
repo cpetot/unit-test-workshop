eb20146290aa9201f9e8ddf6c24d00395d050a2a
cpetot <claude.petot@gmail.com>
2017-05-15 15:01:06 +0200

    Correction exercice 3

--- src/test/java/com/cpetot/service/UserServiceTest.java
+++ src/test/java/com/cpetot/service/UserServiceTest.java
@@ -1,10 +1,20 @@
 package com.cpetot.service;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyZeroInteractions;
+import static org.mockito.Mockito.when;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import com.cpetot.entities.User;
+import com.cpetot.exceptions.UserNameNotFoundException;
 import com.cpetot.repository.MovieRepository;
 import com.cpetot.repository.UserRepository;
 import com.cpetot.services.MovieService;
@@ -26,6 +36,57 @@ public class UserServiceTest {
 	private UserService userService;
 
 	// Tests de la méthode getUser
+	@Test
+	public void shouldThrowException_WhenGetUser_WithUsernameNull() {
+		// Given
+		String username = null;
+
+		try {
+			// When
+			userService.getUser(username);
+			Assertions.fail("We should not return any user");
+		}
+		catch (IllegalArgumentException e) {
+			// Then
+			assertThat(e)
+					.hasMessage("Username must not be null");
+		}
+
+		verifyZeroInteractions(userRepository);
+	}
+
+	@Test
+	public void shouldThrowException_WhenGetUser_WithUserNotFound() {
+		// Given
+		String username = "foo";
+
+		try {
+			// When
+			userService.getUser(username);
+			Assertions.fail("We should not return any user");
+		}
+		catch (UserNameNotFoundException e) {
+			// Then
+			assertThat(e)
+					.hasMessage("User with username foo does not exist");
+		}
+
+		verify(userRepository).findByUsername(username);
+	}
+
+	@Test
+	public void shouldReturnUser_WhenGetUser_WithUserFound() {
+		// Given
+		String username = "foo";
+		User user = mock(User.class);
+		when(userRepository.findByUsername(username)).thenReturn(user);
+
+		// When
+		User result = userService.getUser(username);
+
+		assertThat(result).isEqualTo(user);
+		verify(userRepository).findByUsername(username);
+	}
 
 	// Tests de la méthode addToWatchList
 
