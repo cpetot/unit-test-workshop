13ca4dd0c4a9905ab676fe1b86220b70d5cc7a43
cpetot <claude.petot@gmail.com>
2017-05-15 15:22:22 +0200

    Ajout des autres tests

--- src/test/java/com/cpetot/service/UserServiceTest.java
+++ src/test/java/com/cpetot/service/UserServiceTest.java
@@ -1,10 +1,13 @@
 package com.cpetot.service;
 
+import static java.util.Arrays.asList;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyZeroInteractions;
 import static org.mockito.Mockito.when;
+
+import java.util.List;
 
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
@@ -13,6 +16,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import com.cpetot.entities.Movie;
 import com.cpetot.entities.User;
 import com.cpetot.exceptions.UserNameNotFoundException;
 import com.cpetot.repository.MovieRepository;
@@ -89,6 +93,43 @@ public class UserServiceTest {
 	}
 
 	// Tests de la méthode addToWatchList
+	@Test
+	public void shouldAddMovieToUserWatchList_WhenaddToWatchList() {
+		// Given
+		String username = "foo";
+		User user = mock(User.class);
+		String movieTitle = "The Lord of the Rings";
+		Movie movie = mock(Movie.class);
+
+		when(userRepository.findByUsername(username)).thenReturn(user);
+		when(movieService.getByTitle(movieTitle)).thenReturn(movie);
+
+		// When
+		userService.addToWatchList(username, movieTitle);
+
+		// Then
+		verify(userRepository).findByUsername(username);
+		verify(movieService).getByTitle(movieTitle);
+		verify(userRepository).save(user);
+
+	}
 
 	// Tests de la méthode getWatchList
+	@Test
+	public void shouldReturnUserWatchList_WhenGetWatchList() {
+		// Given
+		String username = "foo";
+		User user = mock(User.class);
+		when(userRepository.findByUsername(username)).thenReturn(user);
+
+		List<Movie> movies = asList(mock(Movie.class), mock(Movie.class));
+		when(user.getWatchList()).thenReturn(movies);
+
+		// When
+		List<Movie> result = userService.getWatchList(username);
+
+		// Then
+		verify(userRepository).findByUsername(username);
+		assertThat(result).isEqualTo(movies);
+	}
 }
