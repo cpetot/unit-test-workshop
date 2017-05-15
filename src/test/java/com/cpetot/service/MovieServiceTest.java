f752de7d226a1898716f8d4f1b90a7f41968ab73
cpetot <claude.petot@gmail.com>
2017-05-15 14:40:26 +0200

    Modification des préparations de test
    - Suppression de AbstractMockTest
    - Initialisation pour le premier service testé

--- src/test/java/com/cpetot/service/MovieServiceTest.java
+++ src/test/java/com/cpetot/service/MovieServiceTest.java
@@ -1,26 +1,6 @@
 package com.cpetot.service;
 
-import org.junit.Rule;
-import org.junit.rules.ExpectedException;
-import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.runners.MockitoJUnitRunner;
-
-import com.cpetot.repository.MovieRepository;
-import com.cpetot.services.MovieService;
-
-@RunWith(MockitoJUnitRunner.class)
 public class MovieServiceTest {
-
-	@Mock
-	private MovieRepository movieRepository;
-
-	@InjectMocks
-	private MovieService movieService;
-
-	@Rule
-	public ExpectedException thrown = ExpectedException.none();
 
 	// Tests de la méthode getByTitle
 
--- src/test/java/com/cpetot/service/UserServiceTest.java
+++ src/test/java/com/cpetot/service/UserServiceTest.java
@@ -1,12 +1,42 @@
 package com.cpetot.service;
 
-import com.cpetot.AbstractMockTest;
+import static org.junit.rules.ExpectedException.none;
 
-public class UserServiceTest extends AbstractMockTest {
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
+import com.cpetot.repository.MovieRepository;
+import com.cpetot.repository.UserRepository;
+import com.cpetot.services.MovieService;
+import com.cpetot.services.UserService;
+
+public class UserServiceTest {
+
+	@Rule
+	public ExpectedException thrown = none();
+
+	@Mock
+	private UserRepository userRepository;
+
+	@Mock
+	private MovieRepository movieRepository;
+
+	@Mock
+	private MovieService movieService;
+
+	@InjectMocks
+	private UserService userService;
+
+	// Tests de la méthode getUser
+	@Test
+	public void shouldThrowException_WhenGetUser_WithUsernameNull() {
+
+	}
 
 	// Tests de la méthode addToWatchList
 
 	// Tests de la méthode getWatchList
-
-	// Tests de la méthode getUser
 }
